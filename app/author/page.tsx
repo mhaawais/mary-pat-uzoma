69fceda6ea34eb9b81f42a1c9d3c821189e62b29
Muhammad Awais <161104713+mhaawais@users.noreply.github.com>
2025-04-18 23:39:38 +0500

    Update page.tsx

--- app/author/page.tsx
+++ app/author/page.tsx
@@ -117,7 +117,7 @@ const Author = () => {
               />
               {/* Overlay */}
               <div className="absolute inset-0 bg-black bg-opacity-40 opacity-0 group-hover:opacity-100 transition-opacity duration-300 flex items-center justify-center">
-                <p className="text-white text-sm font-semibold"></p>
+              
               </div>
             </div>
           ))}
